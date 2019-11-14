50fffde2d73cf14b04667c801959613e0d5770e0
heinrich <heinrich_asr@web.de>
2019-11-14 14:22:15 +0100

    fix include

--- src/helper/MathHelper.cpp
+++ src/helper/MathHelper.cpp
@@ -16,6 +16,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 */
 
 #include "helper/MathHelper.h"
+#include <random>
 
 namespace ProbabilisticSceneRecognition {
  
